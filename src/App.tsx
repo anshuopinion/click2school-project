996eb428dbbc004f16ee039a82754e9c6ac91891
Anshu Raj <anshusopinion@gmail.com>
2022-03-12 15:11:43 +0530

    jvector map package installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+/* The goal of the project is to create a ReactJS based frontend
+ application which lets a user view the teachers on a map.
+ jVectorMap library should be used for working with map.
+
+*/
 const App = () => {
   return <div>App</div>;
 };
